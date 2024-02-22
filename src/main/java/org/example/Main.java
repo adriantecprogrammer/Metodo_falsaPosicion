fe43d12cb6a100a8bc70313f86d2a83e4e3ca02d
adriantecprogrammer <adrian2004mexico.com@gmail.com>
2024-02-21 22:09:13 -0600

    feat: se agregaron validaciones Se agregaron validaciones que mejoran la experiencia de usuario

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,19 @@
 package org.example;
 
 import org.nfunk.jep.JEP;
+import org.nfunk.jep.function.NaturalLogarithm;
 
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Configurar el objeto JEP
         double tolerance = 0.00001;
         int maxIterations = 10000;
         JEP jep = new JEP();
         jep.addStandardFunctions();
         jep.addStandardConstants();
+        jep.addFunction("ln", new NaturalLogarithm());
         double x = 0;
         double x2 = 0;
         double result1 = 0;
@@ -20,38 +21,41 @@ public class Main {
 
         // Pedir al usuario que ingrese la expresión matemática
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Ingresa la expresión matemática (por ejemplo, x^2-2, e^x: ");
+        System.out.print("Ingresa la expresión matemática (por ejemplo, x^2-2, e^(2*x)-3, ln(2*x): ");
         String expresion = scanner.nextLine();
 
         boolean band = false;
 
         do {
-            band = true;
-            System.out.print("Ingresa el valor de la frontera a: ");
-            if (scanner.hasNextDouble()) {
+            try {
+
+                band = true;
+                System.out.print("Ingresa el valor de la frontera a: ");
+
                 x = scanner.nextDouble();
                 jep.addVariable("x", x);
                 jep.parseExpression(expresion);
                 result1 = jep.getValue();
-            } else {
-                System.err.println("Error: Ingresa un valor numérico para x.");
-                return;
-            }
-            System.out.print("Ingresa el valor de la frontera b: ");
-            if (scanner.hasNextDouble()) {
+
+
+                System.out.print("Ingresa el valor de la frontera b: ");
+
                 x2 = scanner.nextDouble();
                 jep.addVariable("x", x2);
                 jep.parseExpression(expresion);
                 result2 = jep.getValue();
-            } else {
-                System.err.println("Error: Ingresa un valor numérico para x.");
-                return;
+
+
+                if (result1 * result2 > 0) {
+                    System.out.println("Upss elige otro valor para las fronteras");
+                    band = false;
+                }
+            }catch (Exception e){
+                System.out.println("Upss ocurrio un error");
+                band=false;
+                scanner.next();
             }
 
-            if (result1 * result2 > 0) {
-                System.out.println("Upss elige otro valor para las fronteras");
-                band = false;
-            }
         } while (!band);
 
         double root = falsaPosicion(expresion, jep, x, x2, tolerance, maxIterations);
